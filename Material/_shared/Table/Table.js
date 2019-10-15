e16b591adcc20069558a32890d48754f7e9294fe
itechdom <oalghnmi@gmail.com>
2019-10-15 14:31:40 -0400

    Table

--- Material/_shared/Table/Table.js
+++ Material/_shared/Table/Table.js
@@ -2,7 +2,6 @@ import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/styles";
-// import theme from "Theme";
 import DeleteIcon from "@material-ui/icons/Delete";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import theme from "Theme";
