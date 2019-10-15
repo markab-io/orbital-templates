671a22d21ad58e6eb6980c1326b7caddbfb39751
itechdom <oalghnmi@gmail.com>
2019-10-15 12:55:59 -0400

    new build

--- Material/_shared/FloatingAddButton/FloatingAddButton.js
+++ Material/_shared/FloatingAddButton/FloatingAddButton.js
@@ -2,16 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/styles";
-// import theme from "Theme";
+import theme from "Theme";
 import AddIcon from "@material-ui/icons/Add";
 import { Typography, Button, Fab } from "@material-ui/core";
 
 const styles = theme => {
-  console.log("theme", theme);
   return {
     fab: {
       bottom: "0px",
-      right: theme && theme.spacing(2)
+      right: theme && theme.spacing && theme.spacing(2)
     }
   };
 };
@@ -36,4 +35,4 @@ FloatingAddButton.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(FloatingAddButton);
+export default withStyles(styles, { defaultTheme: theme })(FloatingAddButton);
