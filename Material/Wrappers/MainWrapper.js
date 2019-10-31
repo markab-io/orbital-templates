d4fe0590b800e59f20919e6b6efba8f3e3494596
itechdom <oalghnmi@gmail.com>
2019-10-31 12:55:44 -0400

    add option for lower tab

--- Material/Wrappers/MainWrapper.js
+++ Material/Wrappers/MainWrapper.js
@@ -49,7 +49,9 @@ const MainWrapper = props => {
     menuOpen,
     setMenuOpen,
     classes,
-    noMargin
+    noMargin,
+    isTabMenu,
+    onRouteClick
   } = props;
   const isAnchor = Boolean(anchorEl);
   let route = routeList.filter(({ name, url }) => {
@@ -62,82 +64,155 @@ const MainWrapper = props => {
     <>
       <CssBaseline />
       <div className={classes.root}>
-        <AppBar>
-          <Toolbar className={classes.toolbar}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={() => setOpen(true)}
-              className={classNames(
-                classes.menuButton,
-                open && classes.menuButtonHidden
+        {!isTabMenu && (
+          <AppBar>
+            <Toolbar className={classes.toolbar}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={() => setOpen(true)}
+                className={classNames(
+                  classes.menuButton,
+                  open && classes.menuButtonHidden
+                )}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="title"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
+                {brand
+                  ? brand
+                  : (routeList[currentRoute] && routeList[currentRoute].name) ||
+                    routeList[0].name}
+              </Typography>
+              {auth && (
+                <div>
+                  <Tooltip title={(user && user.name) || ""}>
+                    <IconButton
+                      aria-owns={isAnchor ? "menu-appbar" : null}
+                      aria-haspopup="true"
+                      onClick={event => {
+                        setAnchorEl(event.currentTarget);
+                      }}
+                      color="inherit"
+                    >
+                      <img
+                        style={{ borderRadius: "30px" }}
+                        src={user && user.image}
+                        width={"40px"}
+                        height={"auto"}
+                        alt="Profile"
+                      />
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left"
+                    }}
+                    transformOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left"
+                    }}
+                    open={isAnchor}
+                    onClose={() => setAnchorEl(null)}
+                  >
+                    <MenuItem
+                      onClick={event => {
+                        onLogout();
+                        setMenuOpen(false);
+                        history.push("/auth/login");
+                      }}
+                    >
+                      Log out
+                    </MenuItem>
+                  </Menu>
+                </div>
               )}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="title"
-              color="inherit"
-              noWrap
-              className={classes.title}
-            >
-              {brand
-                ? brand
-                : (routeList[currentRoute] && routeList[currentRoute].name) ||
-                  routeList[0].name}
-            </Typography>
-            {auth && (
-              <div>
-                <Tooltip title={(user && user.name) || ""}>
-                  <IconButton
-                    aria-owns={isAnchor ? "menu-appbar" : null}
-                    aria-haspopup="true"
-                    onClick={event => {
-                      setAnchorEl(event.currentTarget);
-                    }}
-                    color="inherit"
-                  >
-                    <img
-                      style={{ borderRadius: "30px" }}
-                      src={user && user.image}
-                      width={"40px"}
-                      height={"auto"}
-                      alt="Profile"
-                    />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left"
-                  }}
-                  transformOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left"
-                  }}
-                  open={isAnchor}
-                  onClose={() => setAnchorEl(null)}
-                >
-                  <MenuItem
-                    onClick={event => {
-                      onLogout();
-                      setMenuOpen(false);
-                      history.push("/auth/login");
-                    }}
-                  >
-                    Log out
-                  </MenuItem>
-                </Menu>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
+            </Toolbar>
+          </AppBar>
+        )}
         <>
           <Divider />
           <Hidden mdUp>
+            {!isTabMenu && (
+              <Tabs
+                className={classes.tabs}
+                value={currentRoute || 0}
+                onChange={(event, route) => {
+                  history.push(routeList[route].url);
+                }}
+                variant="scrollable"
+                indicatorColor="primary"
+                textColor="primary"
+                scrollButtons="on"
+                aria-label="scrollable force tabs example"
+              >
+                {routeList.map((route, index) => {
+                  return (
+                    <Tab
+                      label={route.name}
+                      icon={<Icon>{route.icon}</Icon>}
+                      key={index}
+                      button
+                    />
+                  );
+                })}
+              </Tabs>
+            )}
+          </Hidden>
+          {!isTabMenu && (
+            <Drawer open={open} onClose={() => setOpen(false)}>
+              <Routes
+                currentRoute={currentRoute || 0}
+                routeList={routeList}
+                isLoggedIn={user && !!user.name}
+                onClick={route => {
+                  setOpen(false);
+                  history.push(route.url);
+                }}
+              />
+            </Drawer>
+          )}
+          {!isTabMenu && (
+            <Hidden smDown>
+              <List
+                style={{
+                  position: "fixed",
+                  marginTop: noMargin ? "0px" : appBarHeight,
+                  maxHeight: "680px",
+                  overflowY: "scroll"
+                }}
+                className={classes.sidebar}
+              >
+                <Routes
+                  currentRoute={currentRoute || 0}
+                  routeList={routeList}
+                  isLoggedIn={user && !!user.name}
+                  onClick={route => history.push(route.url)}
+                />
+              </List>
+            </Hidden>
+          )}
+        </>
+        <main style={{ marginBottom: "100px" }} className={classes.hasPadding}>
+          {children}
+        </main>
+        {isTabMenu && (
+          <AppBar
+            style={{
+              top: "auto",
+              bottom: 0,
+              backgroundColor: "white"
+            }}
+          >
             <Tabs
               className={classes.tabs}
               value={currentRoute || 0}
@@ -161,40 +236,8 @@ const MainWrapper = props => {
                 );
               })}
             </Tabs>
-          </Hidden>
-          <Drawer open={open} onClose={() => setOpen(false)}>
-            <Routes
-              currentRoute={currentRoute || 0}
-              routeList={routeList}
-              isLoggedIn={user && !!user.name}
-              onClick={route => {
-                setOpen(false);
-                history.push(route.url);
-              }}
-            />
-          </Drawer>
-          <Hidden smDown>
-            <List
-              style={{
-                position: "fixed",
-                marginTop: noMargin ? "0px" : appBarHeight,
-                maxHeight: "680px",
-                overflowY: "scroll"
-              }}
-              className={classes.sidebar}
-            >
-              <Routes
-                currentRoute={currentRoute || 0}
-                routeList={routeList}
-                isLoggedIn={user && !!user.name}
-                onClick={route => history.push(route.url)}
-              />
-            </List>
-          </Hidden>
-        </>
-        <main style={{ marginBottom: "100px" }} className={classes.hasPadding}>
-          {children}
-        </main>
+          </AppBar>
+        )}
       </div>
     </>
   );
